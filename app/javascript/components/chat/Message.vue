a2808cd999096c6c7ba1a06297d16aa7051dafd3
Gianpiero Addis <gianpiero.addis@gmail.com>
2021-01-02 01:49:50 +0100

    Add audio player

--- app/javascript/components/chat/Message.vue
+++ app/javascript/components/chat/Message.vue
@@ -6,6 +6,13 @@
         <div v-if="message.media_mime_type == 'image/jpeg'">
           <img class="mt-2" :src="'data:image/jpeg;base64,' + message.message_thumbnail_base64" alt="Image"/>
         </div>
+        <div v-if="message.media_mime_type == 'audio/ogg; codecs=opus'">
+          <audio controls>
+            <!-- TODO: calculate the correct path to the audio file. -->
+            <source src="foobar.ogg" type="audio/ogg">
+          Your browser does not support the audio element.
+          </audio>
+        </div>
         <p class="text-sm mt-2">{{ body }}</p>
         <p class="text-right text-xs text-gray-700 mt-1">{{ sentAt }}</p>
       </div>
@@ -28,12 +35,6 @@ export default {
 
     body() {
       return this.message.data ?? this.message.media_caption;
-    }
-  },
-
-  methods: {
-    isNumeric(string) {
-      return !isNaN(parseFloat(string));
     }
   }
 };
